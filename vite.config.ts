2ffe78128d914fbbb7dd6314b1a23149b4f30758
cg-ghost <ghostappwallet@gmail.com>
2024-10-19 15:05:15 -0500

    revert icons

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,8 @@ const manifest: ManifestV3Export = {
   name: 'Ghost',
   version: '0.0.0.4',
   icons: {
+    48: 'assets/ghost-outline-thick-48.png',
+    128: 'assets/ghost-outline-128.png',
     512: 'assets/ghost-outline-512.png',
   },
   action: {
