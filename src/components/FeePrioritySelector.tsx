8f0867fd7bd9a0fec77832a9e5ae5bfbfc2bf618
kaswallet <ghostappwallet@gmail.com>
2024-10-24 16:20:58 -0500

    update selected fee UI to show minutes vs seconds

--- src/components/FeePrioritySelector.tsx
+++ src/components/FeePrioritySelector.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FEE_TYPES } from '@/utils/constants'
+import { ArrowUturnRightIcon } from '@heroicons/react/24/solid'
 
 interface FeePrioritySelectorProps {
   currentFeeTypeIndex: number
@@ -21,18 +22,26 @@ const FeePrioritySelector: React.FC<FeePrioritySelectorProps> = ({
 
   return (
     <>
-      <div className="w-full text-left text-mutedtext font-lato font-light text-base px-6">
-        <span
-          className={`font-bold ${isButtonEnabled ? 'text-primary hover:cursor-pointer' : 'text-mutedtext'}`}
+      <div className="w-full text-left text-mutedtext font-lato font-light text-base px-4 pt-2">
+        <div
+          className={`${
+            isButtonEnabled ? 'bg-primary hover:cursor-pointer' : 'bg-muted'
+          } text-secondarytext rounded-lg px-2 py-1 inline-flex items-center`}
           onClick={isButtonEnabled ? onFeeTypeClick : undefined}
+          style={{ userSelect: 'none' }} // Prevent text selection
         >
-          Fee priority: {feeTypeText.charAt(0).toUpperCase() + feeTypeText.slice(1)} ~{' '}
-          {estimatedSeconds >= 60
-            ? `${parseFloat((estimatedSeconds / 60).toFixed(2))} minute${parseFloat((estimatedSeconds / 60).toFixed(2)) !== 1 ? 's' : ''}`
-            : `${parseFloat(estimatedSeconds.toFixed(2))} second${parseFloat(estimatedSeconds.toFixed(2)) !== 1 ? 's' : ''}`}
-        </span>
+          <ArrowUturnRightIcon className="h-5 w-5 mr-1" />
+          <span className="font-bold">
+            {feeTypeText.charAt(0).toUpperCase() + feeTypeText.slice(1)} ~{' '}
+            {estimatedSeconds >= 60
+              ? `${parseFloat((estimatedSeconds / 60).toPrecision(2))} minute${parseFloat((estimatedSeconds / 60).toPrecision(2)) !== 1 ? 's' : ''}`
+              : estimatedSeconds < 1
+                ? `${parseFloat(estimatedSeconds.toPrecision(1))} second${parseFloat(estimatedSeconds.toPrecision(1)) !== 1 ? 's' : ''}`
+                : `${parseFloat(estimatedSeconds.toPrecision(1))} second${parseFloat(estimatedSeconds.toPrecision(1)) !== 1 ? 's' : ''}`}
+          </span>
+        </div>
       </div>
-      <div className="w-full text-left text-mutedtext font-lato font-light text-base px-6">
+      <div className="w-full text-left text-mutedtext font-lato font-light text-base px-4 pb-4">
         Fee: {estimatedFee ? `${estimatedFee} KAS` : <span className="invisible">Fee Placeholder</span>}
       </div>
     </>
