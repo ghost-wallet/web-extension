58735073ae6a3d4443fd526a4ce98d4e64195949
phantomofthechromium <phantomofthechromium@protonmail.com>
2024-10-29 15:48:44 -0500

    Remove unneeded useEffect in AnimatedMain

--- src/components/AnimatedMain.tsx
+++ src/components/AnimatedMain.tsx
@@ -13,19 +13,10 @@ interface AnimatedMainProps {
 const AnimatedMain: React.FC<AnimatedMainProps> = ({ children, className, showConnectingMessage = true }) => {
   const { kaspa } = useKaspa()
   const { settings } = useSettings()
-  const [isConnecting, setIsConnecting] = useState(false)
-
-  useEffect(() => {
-    if (!kaspa.connected) {
-      setIsConnecting(true)
-    } else {
-      setIsConnecting(false)
-    }
-  }, [kaspa.connected])
 
   return (
     <>
-      {isConnecting && showConnectingMessage && (
+      {!kaspa.connected && showConnectingMessage && (
         <div className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center bg-slightmuted text-primarytext font-lato text-sm p-1">
           <p className="pl-4">{`Connecting to ${settings.nodes[settings.selectedNode].address}...`}</p>
           <div className="p-1">
