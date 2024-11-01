84db43eab68dedd54c7d5eac0a60b3ca3d73f25c
cg-ghost <ghostappwallet@gmail.com>
2024-10-31 20:07:16 -0500

    fix Receive page for when address is not there

--- src/pages/Wallet/Receive.tsx
+++ src/pages/Wallet/Receive.tsx
@@ -8,6 +8,7 @@ import Header from '@/components/Header'
 
 export default function Receive() {
   const { kaspa } = useKaspa()
+  const kaspaAddress = kaspa.addresses[0] || ''
 
   return (
     <>
@@ -15,12 +16,12 @@ export default function Receive() {
         <Header title="Receive Address" showBackButton={true} />
         <div className="pt-6 pb-20 flex flex-col items-center justify-center relative w-full">
           <div className="p-2 bg-white rounded-lg">
-            <QRCode style={{ height: '150px', width: '150px' }} value={kaspa.addresses[0]} />
+            <QRCode style={{ height: '150px', width: '150px' }} value={kaspaAddress} />
           </div>
 
           <h1 className="pt-4 font-semibold text-base text-primarytext text-center">Your Kaspa Address</h1>
           <div className="py-6 w-full flex justify-center">
-            <KaspaAddress address={kaspa.addresses[0]} />
+            <KaspaAddress address={kaspaAddress} />
           </div>
 
           <p className="px-6 text-base text-mutedtext text-center">
