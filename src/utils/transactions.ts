dc282a09f2b0775664205ba9abdafad64faa7662
kaswallet <ghostappwallet@gmail.com>
2024-11-06 13:41:12 -0600

    update krc20 txn history testnet URLs

--- src/utils/transactions.ts
+++ src/utils/transactions.ts
@@ -1,4 +1,5 @@
 import { KRC20Transaction } from '@/utils/interfaces'
+import useSettings from '@/hooks/contexts/useSettings'
 
 interface OperationDetails {
   operationType: 'Sent' | 'Received' | 'Minted'
@@ -17,8 +18,20 @@ export function getOperationDetails(operation: KRC20Transaction, address: string
   return { operationType, isSent, isReceived, isMint }
 }
 
-export const getKaspaExplorerUrl = (transactionId: string) =>
-  `https://explorer.kaspa.org/txs/${transactionId}`
+export const getKaspaExplorerUrl = (transactionId: string) => {
+  const { settings } = useSettings()
+  const networkAddress = settings.nodes[settings.selectedNode].address
+
+  let baseUrl = 'https://explorer.kaspa.org'
+
+  if (networkAddress.includes('testnet-10')) {
+    baseUrl = 'https://explorer-tn10.kaspa.org'
+  } else if (networkAddress.includes('testnet-11')) {
+    baseUrl = 'https://explorer-tn11.kaspa.org'
+  }
+
+  return `${baseUrl}/txs/${transactionId}`
+}
 
 export const getTransactionStatusText = (operationType: string, opAccept: string, op: string): string => {
   return opAccept === '1' ? operationType : `${op.charAt(0).toUpperCase() + op.slice(1)} Failed`
