a907255b9cf74beed21cfb0a3601493d2e995b1f
cg-ghost <ghostappwallet@gmail.com>
2024-11-11 08:24:50 -0600

    start chainge fetch aggregate quote hook

---
+++ src/hooks/chainge/fetchAggregateQuote.ts
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { ChaingeToken } from '@/hooks/chainge/fetchChaingeTokens'
+
+export interface ChaingeAggregateQuote {
+  chain: string
+  chainDecimal: number
+  aggregator: string
+  outAmount: number
+  outAmountUsd: string
+  minOutAmount?: string
+  serviceFee: string
+  gasFee: string
+  priceImpact: string
+  routeSummary: string
+}
+
+const API_URL = 'https://api2.chainge.finance/v1/getAggregateQuote'
+
+export const fetchChaingeTokens = async (
+  fromToken: ChaingeToken,
+  toToken: ChaingeToken,
+  fromAmount: number,
+): Promise<ChaingeAggregateQuote[]> => {
+  try {
+    const response = await axios.get<{ code: number; msg: string; data: ChaingeAggregateQuote }>(API_URL, {
+      params: {
+        fromAmount,
+        fromTokenAddress: fromToken.contractAddress,
+        fromDecimal: fromToken.decimals,
+        fromChain: 'KAS',
+        toTokenAddress: toToken.contractAddress,
+        toDecimal: toToken.decimals,
+        toChain: 'KAS',
+      },
+    })
+
+    if (response.data.code === 0 && response.data.data) {
+      // return response.data.data
+    } else {
+      throw new Error('Error fetching Chainge tokens: Invalid API response')
+    }
+  } catch (error) {
+    console.error('Error fetching Chainge tokens:', error)
+    throw error
+  }
+}
