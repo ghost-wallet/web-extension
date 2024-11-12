40f23dc610f0a1e423aa5a52b57b6c5af739c34f
phantomofthechromium <phantomofthechromium@protonmail.com>
2024-11-12 15:39:28 -0600

    Add logging to every step

--- src/wallet/exchange/chainge.ts
+++ src/wallet/exchange/chainge.ts
@@ -63,7 +63,12 @@ export default class Chainge {
   }
 
   async submitChaingeOrder({ fromAmount, fromToken, toToken, quote, feeRate }: SubmitChaingeOrderRequest) {
+    
+    console.log('[Chainge] submit', { fromAmount, fromToken, toToken, quote, feeRate })
+    
     const amount = parseUnits(fromAmount, fromToken.decimals).toString()
+
+    console.log('[Chainge] amount', amount)
 
     const channelFeeRate = '0'
 
@@ -76,6 +81,7 @@ export default class Chainge {
 
     const { chain, chainDecimal, outAmount, serviceFee, gasFee, slippage } = quote
     const receiveAmount = BigInt(outAmount) - BigInt(serviceFee) - BigInt(gasFee)
+    console.log('[Chainge] receiveAmount', receiveAmount.toString())
     if (receiveAmount <= BigInt(0)) {
       throw 'The current quote amount cannot cover the fees. Please enter a larger amount.'
     }
@@ -84,16 +90,26 @@ export default class Chainge {
     const receiveAmountHr = formatUnits(receiveAmount, chainDecimal)
     const receiveAmountForExtra = parseUnits(receiveAmountHr, chainDecimal).toString()
 
+    console.log('[Chainge] receiveAmountHr', receiveAmountHr)
+    console.log('[Chainge] receiveAmountForExtra', receiveAmountForExtra)
+
     // Computed minimum, After calculating the minimum value, we need to convert it to the decimals of the target chain.
     const miniAmount = BigNumber(receiveAmountHr)
       .multipliedBy(BigNumber(1 - parseFloat(slippage) * 0.01))
       .toString()
     const miniAmountForExtra = parseUnits(miniAmount, chainDecimal).toString()
 
+    console.log('[Chainge] miniAmount', miniAmount)
+    console.log('[Chainge] miniAmountForExtra', miniAmountForExtra)
+
     // 1_Expected value;2_Third party profit ratio;3_version;4_Mini Amount;5_Execution chain
     const extra = `1_${receiveAmountForExtra};2_${channelFeeRate};3_2;4_${miniAmountForExtra};5_KAS`
 
+    console.log('[Chainge] extra', extra)
+
     const transactionId = await this.sendChaingeTransaction(fromAmount, fromToken, feeRate)
+
+    console.log('[Chainge] transactionId', transactionId)
 
     const sourceCertsObj = {
       amount,
@@ -104,9 +120,20 @@ export default class Chainge {
       fromPublicKey: this.addresses.publicKey.receivePubkeyAsString(0),
       signature: '123456',
     }
+
+    console.log('[Chainge] sourceCertsObj', sourceCertsObj)
+
     const sourceCertsStr = JSON.stringify(sourceCertsObj)
+
+    console.log('[Chainge] sourceCertsStr', sourceCertsStr)
+
     let sourceCertsHex = hexlify(toUtf8Bytes(sourceCertsStr))
+
+    console.log('[Chainge] sourceCertsHex', sourceCertsHex)
+
     sourceCertsHex = sourceCertsHex.substring(2)
+
+    console.log('[Chainge] sourceCertsHex', sourceCertsHex)
 
     const params = {
       sourceCerts: sourceCertsHex,
@@ -121,13 +148,23 @@ export default class Chainge {
       timeout: '0',
       channel: 'chainge',
     }
+
+    console.log('[Chainge] params', params)
+
     const freezeParams = Object.freeze(params)
     let raw = sortParams(freezeParams, fromAddress)
+
+    console.log('[Chainge] raw', raw)
+
     raw = keccak256(hexlify(toUtf8Bytes(raw!)))
+
+    console.log('[Chainge] raw', raw)
 
     const keyGenerator = KeyManager.createKeyGenerator()
     const privateKey = keyGenerator.receiveKey(0)
     const signature = signMessage({ message: raw, privateKey })
+
+    console.log('[Chainge] signature', signature)
 
     const header = {
       Address: fromAddress,
@@ -136,6 +173,8 @@ export default class Chainge {
       Signature: signature,
     }
 
+    console.log('[Chainge] header', header)
+
     const response = await axios.post<AggregateSwapResponse>(API_SUBMIT_ORDER_URL, params, {
       headers: {
         'Content-Type': 'application/json',
@@ -143,12 +182,14 @@ export default class Chainge {
       },
     })
 
-    console.log(response)
+    console.log('[Chainge] response', response)
+    console.log('[Chainge] response.data', response.data)
 
-    return response
+    return response.data
   }
 
   private async sendChaingeTransaction(fromAmount: string, fromToken: ChaingeToken, feeRate: number) {
+    console.log('[Chainge] sendChaingeTransaction', {fromAmount, fromToken, feeRate})
     if (fromToken.contractAddress === '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee') {
       // KAS
       const [transactions] = await this.transactions.create(
@@ -156,7 +197,9 @@ export default class Chainge {
         feeRate,
         '0',
       )
+      console.log('[Chainge] transactions', transactions)
       const submittedTransactions = await this.transactions.submitKaspaTransaction(transactions)
+      console.log('[Chainge] submittedTransactions', submittedTransactions)
       return submittedTransactions[submittedTransactions.length - 1]
     } else {
       // KRC-20
@@ -170,7 +213,9 @@ export default class Chainge {
       }
 
       const info = await this.krc20Transactions.getKRC20Info(toAddress, krc20Token, fromAmount)
+      console.log('[Chainge] info', info)
       const [commitId, revealId] = await this.krc20Transactions.submitKRC20Transaction(info, feeRate)
+      console.log('[Chainge] revealId', revealId)
       return revealId
     }
   }
