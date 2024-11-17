f24a453f4f9073ecf4ace813e3cbabffcde0347d
cg-ghost <ghostappwallet@gmail.com>
2024-11-16 20:40:24 -0600

    set minimum swap amount to $5

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -2,7 +2,7 @@ export const FEE_TYPES = ['slow', 'standard', 'fast'] as const
 
 export const KRC20_COMMIT_AMOUNT = '1' as const
 
-export const MINIMUM_RECEIVE_AMOUNT_USD = 0.1
+export const MINIMUM_RECEIVE_AMOUNT_USD = 5.0
 export const unsupportedChaingeTokens = ['USDC', 'BTC', 'ETH', 'XCHNG', 'RTO']
 export const chaingeMinterAddresses = {
   KAS: 'kaspa:qpgmt2dn8wcqf0436n0kueap7yx82n7raurlj6aqjc3t3wm9y5ssqtg9e4lsm',
