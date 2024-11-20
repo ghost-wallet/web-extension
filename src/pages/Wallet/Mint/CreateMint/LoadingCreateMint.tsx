e0a238efc97bd53d8a9ac6f385f0959df6900586
kaswallet <ghostappwallet@gmail.com>
2024-11-20 16:10:08 -0600

    show warning on 5%+ loss

--- src/pages/Wallet/Mint/CreateMint/LoadingCreateMint.tsx
+++ src/pages/Wallet/Mint/CreateMint/LoadingCreateMint.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const LoadingPlaceholder: React.FC<{ className: string }> = ({ className }) => (
-  <div className={`bg-gray-300 animate-pulse ${className}`} />
+  <div className={`bg-muted animate-pulse ${className}`} />
 )
 
 export default function LoadingCreateMint() {
