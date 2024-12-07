b4e282eb37f19f35c75eec2f0f3bd867dc175c44
kj16609 <bigskyman02@gmail.com>
2024-12-06 22:09:10 -0500

    Prepares close button for sidebar

--- src/components/buttons/SidebarButton.tsx
+++ src/components/buttons/SidebarButton.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { SidebarOpenIcon } from 'lucide-react'
+import { SidebarCloseIcon, SidebarOpenIcon } from 'lucide-react'
 
 const SidebarButton: React.FC = () => {
+  const [isOpen, setIsOpen] = React.useState(false)
+
   const handleSidebarOpen = async () => {
+    // open side panel here
+    console.log('Sidebar button open')
   }
 
   return (
     <button onClick={handleSidebarOpen} className="flex items-center">
-      <SidebarOpenIcon className="h-6 w-6 transform transition-transform duration-300 hover:scale-125 text-mutedtext" />
+      {isOpen ? (
+        <SidebarCloseIcon className="h-6 w-6 transform transition-transform duration-300 hover:scale-125 text-mutedtext" />
+      ) : (
+        <SidebarOpenIcon className="h-6 w-6 transform transition-transform duration-300 hover:scale-125 text-mutedtext" />
+      )}
     </button>
   )
 }
